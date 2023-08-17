e9e766b4dec211ddbefbd02ac5c4930e3d0ca586
Jane Tung <22017723@myrp.edu.sg>
2023-08-17 14:40:39 +0800

    Note.java

--- app/src/main/java/com/myapplicationdev/databaserevision/Note.java
+++ app/src/main/java/com/myapplicationdev/databaserevision/Note.java
@@ -3,7 +3,41 @@ package com.myapplicationdev.databaserevision;
 public class Note {
     private int id;
     private String content;
+
     private String priority;
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public Note(int id, String content, String priority) {
+        this.id = id;
+        this.content = content;
+        this.priority = priority;
+    }
+
+    public String toString() {
+        return id + ". " + content + " " + priority;
+    }
 
     //TODO: What should be here?
 
