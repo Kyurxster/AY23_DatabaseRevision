e1be1ec3bc036f9d2a11199734ecc9a0a487f7d9
Jane Tung <22017723@myrp.edu.sg>
2023-08-17 14:54:09 +0800

    InsertActivity.java

--- app/src/main/java/com/myapplicationdev/databaserevision/InsertActivity.java
+++ app/src/main/java/com/myapplicationdev/databaserevision/InsertActivity.java
@@ -33,8 +33,8 @@ public class InsertActivity extends AppCompatActivity {
                 // Create the DBHelper object, passing in the activity's Context
                 DBHelper db = new DBHelper(InsertActivity.this);
 
-
-                db.insertTask();
+                //priority in int??
+                db.insertTask(etContent.getText().toString(), Integer.parseInt(etPriority.getText().toString()));
                 db.close();
             }
         });
